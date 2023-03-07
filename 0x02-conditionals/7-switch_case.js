c193ab644830a5f1d5a4ce0619838143194d57a3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 15:42:21 +0300

    Switch keyword

---
+++ 0x02-conditionals/7-switch_case.js
@@ -0,0 +1,19 @@
+/*
+- The switch keyword with multiple cases.
+*/
+
+let athleteFinalPosition = "first place";
+switch (athleteFinalPosition) {
+  case "first place":
+    console.log("You get the gold medal!");
+    break;
+  case "second place":
+    console.log("You get the silver medal!");
+    break;
+  case "third place":
+    console.log("You get the bronze medal!");
+    break;
+  default:
+    console.log("No medal awarded.");
+    break;
+}
