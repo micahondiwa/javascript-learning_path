213dc805286590c4666be2ef8edc0885567f0cdd
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 13:57:24 +0300

    String concatenation

---
+++ 0x01-variables/5-string_concatenation.js
@@ -0,0 +1,7 @@
+/*
+- string concatenation: A new variable called favoriteAnimal and concatenate with another string.
+*/
+
+let favoriteAnimal = "cow";
+console.log(favoriteAnimal);
+console.log("My favorite animal: " + favoriteAnimal);
