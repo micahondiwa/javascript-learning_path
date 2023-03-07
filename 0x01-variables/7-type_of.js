73138f2aeeadc02303fb71fcf31f80f5a42b6c75
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 14:05:06 +0300

    Operator: typeof

---
+++ 0x01-variables/7-type_of.js
@@ -0,0 +1,8 @@
+/*
+- using typeof operator to determine the tyepe of variable.
+*/
+
+let newVariable = "Playing around with typeof.";
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
