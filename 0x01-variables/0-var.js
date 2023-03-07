af4aeadce44fc54b5ee8114d8d0ddc8c728d0f21
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 13:43:13 +0300

    Creating a new variable with the var keyword

---
+++ 0x01-variables/0-var.js
@@ -0,0 +1,9 @@
+/*
+Declaring a new variable
+*/
+var favoriteFood = "Pizza";
+var numOfSlices = 8;
+
+//Printing out the values of the variables
+console.log(favoriteFood);
+console.log(numOfSlices);
