89921038fff7f211f884d41c969fd2427f6f4368
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 13:52:37 +0300

    Lost and gained dollars

---
+++ 0x01-variables/4-decrease_increase.js
@@ -0,0 +1,10 @@
+/*
+- using the decreament and increament operators to create new variables; gainedDollar and lostDollar.
+*/
+
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
+console.log(gainedDollar);
+console.log(lostDollar);
