3087c9e02601f86487fd13ad6ad34be6b3a68257
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 13:47:48 +0300

    Const

---
+++ 0x01-variables/1-let.js
@@ -0,0 +1,6 @@
+/*
+Creating a new variable with the let keyword.
+*/
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
---
+++ 0x01-variables/2-const.js
@@ -0,0 +1,6 @@
+/*
+- creating a new variable with const keyword.
+- confirming that the value of a const cannot be re-assigned.
+*/
+const entree = "Enchiladas";
+entree = "Tacos";
