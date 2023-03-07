65a8464e1eac3b1a68f8b51b2fcdc71ff1f38409
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 15:22:50 +0300

    Truthy and falsy

---
+++ 0x02-conditionals/3-trythy_falsy.js
@@ -0,0 +1,18 @@
+/*
+- logical operators: truthy/falsy
+*/
+let wordCount = 2;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log("Better get to work!");
+}
+
+let favoritePhrase = "";
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log("This string is definitely empty.");
+}
