adad122944f560a1554478e9ccf95ced9a2e4f5f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 14:07:33 +0300

    Kelvin weather

---
+++ 0x01-variables/8-kelvin_weather.js
@@ -0,0 +1,12 @@
+//Variable kelvin with value 293
+let kelvin = 0;
+//Converting kelvin to celcius
+celcius = kelvin - 273;
+//Converting celcius to fahrenheit
+fahrenheit = celcius * (9 / 5) + 32;
+//Rounding to whole number
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit`);
+newton = celcius * (33 / 100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees newton.`);
