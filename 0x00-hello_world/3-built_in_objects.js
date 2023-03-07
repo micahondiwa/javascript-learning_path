d3db966c168ce5510a1bab33a0d00e0d100349de
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-07 13:29:38 +0300

    Bult-in objects

---
+++ 0x00-hello_world/3-built_in_objects.js
@@ -0,0 +1,11 @@
+/*
+Math.random() method
+- Generate a random number between 0 and 1 then multiply by 100.
+- Converting the output to a whole number.
+- Returning the smallest integer greater than or equal to a decimal number.
+ - a method on the built-in Number object that checks if a number is an integer.
+*/
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
