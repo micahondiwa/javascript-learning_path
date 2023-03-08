b6838c4ec6816a4a92a48b07e8d2cc49058fbfc6
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 06:17:51 +0300

    Functions: Greetings and reminder functions

---
+++ 0x03-functions/0-greetings_reminder.js
@@ -0,0 +1,12 @@
+/*
+- Implementing 2 simple functions 
+in js: One that prints a remeinder, and the other that prints a greeting.
+*/
+function getReminder() {
+  console.log("Water the plants.");
+}
+getReminder();
+function greetInSpanish() {
+  console.log("Buenas tardes.");
+}
+greetInSpanish();
