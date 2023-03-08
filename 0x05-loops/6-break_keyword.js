225ff5a2cd3afc047cd8f3d8333fb089fe106bb9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 08:16:03 +0300

    The break keyword

---
+++ 0x05-loops/6-break_keyword.js
@@ -0,0 +1,14 @@
+/*
+ * The break keyword in a loop.
+ */
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === "Notorious B.I.G.") {
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
