932bb8440fcc7856c880d0a36186d26b55132301
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 08:02:46 +0300

    The while loop

---
+++ 0x05-loops/4-while_loop.js
@@ -0,0 +1,10 @@
+/**
+ * Implenting a while loop.
+ */
+const cards = ["diamond", "spade", "heart", "club"];
+
+let currentCard;
+while (currentCard != "spade") {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
