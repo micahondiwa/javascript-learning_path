4f13f7975d32b9d980c7c14e72b00c9ce39696f2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 06:21:07 +0300

    Say thanks

---
+++ 0x03-functions/1-say_thanks.js
@@ -0,0 +1,9 @@
+/*
+- A function that takes in a name as an input and prints.
+*/
+function sayThanks(name) {
+  console.log(
+    "Thank you for your purchase " + name + "! We appreciate your business."
+  );
+}
+sayThanks("Cole");
