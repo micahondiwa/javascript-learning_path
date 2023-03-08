d3b69bfd0e9fc79d00f99beb0db2ae216244ac2c
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 06:38:08 +0300

    Arrow function

---
+++ 0x03-functions/5-arrow_function.js
@@ -0,0 +1,10 @@
+/*
+- arrow function: returns true if day is Wednesday, otherwise returns false.
+*/
+const plantNeedsWater = (day) => {
+  if (day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+  }
+};
