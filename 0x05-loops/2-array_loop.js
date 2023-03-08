2a78f5a8cb65788e52fc658e69ecd0d9367a0952
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 07:50:29 +0300

    Array loop

---
+++ 0x05-loops/2-array_loop.js
@@ -0,0 +1,8 @@
+/**
+- a for loop that iterates through our vacationSpots array using i as the iterator variable.
+ */
+const vacationSpots = ["Bali", "Paris", "Tulum"];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I would love to visit ${vacationSpots[i]}`);
+}
