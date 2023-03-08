33de536cbd3747ae09ac716fff3f92b291ce5492
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 06:58:56 +0300

    Accessing array elements

---
+++ 0x04-arrays/1-array_elements.js
@@ -0,0 +1,14 @@
+/*
+- accessing elements in an array.
+*/
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life.",
+];
+
+let listItem = famousSayings[0];
+console.log(listItem);
+
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
