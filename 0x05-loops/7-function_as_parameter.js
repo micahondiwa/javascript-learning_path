44cf3e6b8fb463c3833648824da6ea3e3c7596fa
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 08:21:52 +0300

    using functions as parameters

---
+++ 0x05-loops/7-function_as_parameter.js
@@ -0,0 +1,14 @@
+/**
+ * using funtions as parameters
+ */
+const addTwo = (num) => {
+  return num + 2;
+};
+
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 2;
+  let checkB = func(val);
+  return checkA === checkB ? func(val) : "inconsistent results";
+};
+
+console.log(checkConsistentOutput(addTwo, 10));
