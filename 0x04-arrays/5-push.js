27a1d1e8784948ff09fbe20c3c9a1306121cc40b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 07:11:12 +0300

    Push: Adds an eelement to the array using the push method

---
+++ 0x04-arrays/5-push.js
@@ -0,0 +1,6 @@
+/*
+- adding elements to the array using the push method.
+*/
+const chores = ["wash dishes", "do laundry", "take out trash"];
+chores.push("chore 2", "Chore 3");
+console.log(chores);
