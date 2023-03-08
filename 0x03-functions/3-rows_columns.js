fec0da3f7f333ca4e3b001ab4849985384f27049
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 06:29:53 +0300

    Rows by columns

---
+++ 0x03-functions/3-rows_columns.js
@@ -0,0 +1,9 @@
+/*
+- a function that takes number of rows and columns as inputs and returns the product of rows by columns.
+*/
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
