0665f6e8786fba32a246f6a9b2e282b9b28f39d3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 08:36:23 +0300

    List iteration

---
+++ 0x07-higher_order_functions/2-list_iteration.js
@@ -0,0 +1,9 @@
+/**
+ * Iteration: printing a list through iteration.
+ */
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+fruits.forEach(function (item) {
+  console.log("I want to eat a " + item);
+});
