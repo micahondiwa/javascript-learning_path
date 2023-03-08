d5dcc6e2a27534b0acf837371cdd03ec4bf4f13f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-03-08 10:08:55 +0300

    Property assignment

---
+++ 0x06-objects/3-property_assignment.js
@@ -0,0 +1,16 @@
+/**
+ * object property assignments
+ */
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  homePlanet: "Earth",
+  color: "silver",
+  "Secret Mission": "Discover life outside of Earth.",
+};
+
+spaceship.color = "glorious gold";
+
+spaceship.numEngines = 9;
+
+delete spaceship["Secret Mission"];
