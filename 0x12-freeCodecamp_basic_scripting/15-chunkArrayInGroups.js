a327a9facb7ff56fad476232366678e18c68e150
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-20 22:03:22 +0300

    Fix

--- 0x12-freeCodecamp_basic_scripting/15-chunkArrayInGroups.js
+++ 0x12-freeCodecamp_basic_scripting/15-chunkArrayInGroups.js
@@ -5,7 +5,23 @@ two-dimensional array.
 */
 
 function chunkArrayInGroups(arr, size) {
-  return arr;
+  let tempArr = [];
+  const results = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) {
+      tempArr.push(arr[i]);
+    } else {
+      tempArr.push(arr[i]);
+      results.push(tempArr);
+      tempArr = [];
+    }
+  }
+
+  if (tempArr.length !== 0) {
+    results.push(tempArr);
+    return results;
+  }
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
