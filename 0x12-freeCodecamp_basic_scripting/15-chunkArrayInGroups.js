df3d0878367f45477b87c4eaf21685e0b6ee1999
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-20 21:52:00 +0300

    Chunk

---
+++ 0x12-freeCodecamp_basic_scripting/15-chunkArrayInGroups.js
@@ -0,0 +1,11 @@
+/*
+Splits and array (arr) into groups the length of
+size (second arg) and returns them as a 
+two-dimensional array.
+*/
+
+function chunkArrayInGroups(arr, size) {
+  return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
