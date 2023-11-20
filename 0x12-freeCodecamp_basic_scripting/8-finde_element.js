21068a07b4cae125468c649c2b12cc4c53213974
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-20 14:20:31 +0300

    Find element

---
+++ 0x12-freeCodecamp_basic_scripting/8-finde_element.js
@@ -0,0 +1,17 @@
+/* 
+Looks through an array (arr) and returns
+the first element in it that passes a 
+'truth test'.
+*/
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
