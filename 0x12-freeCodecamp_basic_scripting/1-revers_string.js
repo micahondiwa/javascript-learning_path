22382b0c8ac7f98e0ff7280c96f3cb561a9fe14a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-20 11:43:34 +0300

    Reversed str

---
+++ 0x12-freeCodecamp_basic_scripting/1-revers_string.js
@@ -0,0 +1,12 @@
+/*
+A function that reverses a string,
+and returns the reversed string.
+*/
+
+function reverseString(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
