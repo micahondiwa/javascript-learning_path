d6d2668547f0832b2a1ec22b879675541c1a4f5a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-20 14:38:48 +0300

    capitalize the first letter of each word in a sentence

--- 0x12-freeCodecamp_basic_scripting/10-title_case.js
+++ 0x12-freeCodecamp_basic_scripting/10-title_case.js
@@ -3,5 +3,11 @@ Returns the provided string with the first letter
 of each word capitalized.
 */
 function titleCase(str) {
-  return str;
+  const newTitle = str.split(" ");
+  const updatedTitle = [];
+  for (let st in newTitle) {
+    updatedTitle[st] =
+      newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+  }
+  return updatedTitle.join(" ");
 }
