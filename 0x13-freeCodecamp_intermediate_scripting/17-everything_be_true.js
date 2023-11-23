069a48a104656bd2990f785b64d123c121dd627d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-23 20:30:13 +0300

    Truthy

--- 0x13-freeCodecamp_intermediate_scripting/17-everything_be_true.js
+++ 0x13-freeCodecamp_intermediate_scripting/17-everything_be_true.js
@@ -1,3 +1,8 @@
+/*
+Checks if the predicate(second argument) is truthy on all elements of
+a collection (first argument).
+*/
+
 function truthCheck(collection, pre) {
   return collection.every(function (element) {
     return element.hasOwnProperty(pre) && Boolean(element[pre]);
