45deb06a5dba8f53f4384b5eb5ab8135a9ddb696
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-23 20:15:58 +0300

    Flattened Array

---
+++ 0x13-freeCodecamp_intermediate_scripting/15-steamroller.js
@@ -0,0 +1,16 @@
+/*
+Flattens a nested array. 
+Also accounts for varying levels.
+*/
+
+function steamrollArray(arr) {
+  const flattenedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenedArray.push(...steamrollArray(arr[i]));
+    } else {
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+}
