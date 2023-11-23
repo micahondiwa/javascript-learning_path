2aa90e832db6da90c401b504a032d8999a2312cc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-23 11:02:06 +0300

    Fix

--- 0x13-freeCodecamp_intermediate_scripting/4-spinal_tap_case.js
+++ 0x13-freeCodecamp_intermediate_scripting/4-spinal_tap_case.js
@@ -4,9 +4,9 @@ all-lowercase-words-joned-by-dashes.
 */
 
 function spinalCase(str) {
-    let regex = /\S+|_+/gi;
+  let regex = /\S+|_+/gi;
 
-    str = str.replace(/([a-z])([A-Z])gi, "$1 $2");
+  str = str.replace(/([a-z])([A-Z])/gi, "$1 $2");
 
-    return str.replace(regex, "-").toLowerCase();
+  return str.replace(regex, "-").toLowerCase();
 }
