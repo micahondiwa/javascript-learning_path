21c5ea1f997a0cdb7b09603cccae948066024b1a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-23 20:01:40 +0300

    True

---
+++ 0x13-freeCodecamp_intermediate_scripting/14-drop_it.js
@@ -0,0 +1,5 @@
+/*
+Given the array, iterate through and remove each element starting
+from the first element (the 0 index) until the function 'func' returns
+true when the iterated element is passed through.
+*/
