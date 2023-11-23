2431ab56815166b1bef19fa47316539e56853374
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-23 20:03:33 +0300

    Add it

--- 0x13-freeCodecamp_intermediate_scripting/14-drop_it.js
+++ 0x13-freeCodecamp_intermediate_scripting/14-drop_it.js
@@ -3,3 +3,10 @@ Given the array, iterate through and remove each element starting
 from the first element (the 0 index) until the function 'func' returns
 true when the iterated element is passed through.
 */
+
+function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
